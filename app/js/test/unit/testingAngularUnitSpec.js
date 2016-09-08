f561383db11807c786d4de6c870f1b3a633cd842
Chen Wang <chenwang@Chens-MacBook-Pro.local>
2016-09-08 13:27:52 -0500

    added beforeEach()

--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -1,19 +1,20 @@
 describe('Testing AngularJS test suite',function(){
+	beforeEach(module('myAppModule')); //provided by angular-mocks
 	describe('Testing AngularJS Controller',function(){
-		
-		it('should be intialize the title in the scope',function(){
-			module('myAppModule'); //provided by angular-mocks
-			var scope = {};  //initialize empty scope object
-			var ctrl;
-
-			//provided by angular-mocks used to inject angular
+		beforeEach(//provided by angular-mocks used to inject angular
 			//component to our test,in this case we only inject
 			// controller
-			inject(function($controller){		
+			inject(function($controller,$rootScope){		
 				//link the scope variable to $scope in order to access all variables
 				//defined in our angular app.
+				scope = $rootScope.$new();
 				ctrl = $controller('testingController',{$scope:scope})
-			}) 
+			}));
+
+			var scope,ctrl;
+
+		it('should be intialize the title in the scope',function(){
+					
 			//each expectation is made of the value you want to check
 			// and some conditions you want to check against(matcher in Jasmine)
 			expect(scope.title).toBeDefined();
